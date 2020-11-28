dd1c96f4d5f09ac17e199d02f2f40afcafed72e5
Haley Derrod <haley@Haleys-MacBook-Air.local>
2020-11-28 10:49:35 +0100

    solution to assignment 1

---
+++ rasterization.cpp
@@ -0,0 +1,68 @@
+#include"rasterization.h"
+using namespace pipeline3D;
+
+#include<iostream>
+#include<chrono>
+
+
+
+
+
+
+
+
+    struct my_shader : public vertex_shader<char> {
+         char shade(Vertex v) override {
+            return static_cast<char>((v.z-1)*10.0f+0.5f)%10+'0';
+        }
+    };
+
+
+    int main() {
+        const int w=150;
+        const int h=50;
+
+        my_shader shader;
+        Rasterizer<char> rasterizer;
+        rasterizer.set_shader(&shader);
+        rasterizer.set_perspective_projection(-1,1,-1,1,1,2);
+
+        std::vector<char> screen(w*h,'.');
+        rasterizer.set_target(w,h,&screen[0]);
+        constexpr float slope=-0.2f;
+
+        Vertex v1={1,-1,1.5f+slope*(1-1),0,0,0,0,0}, v2={1,1,1.5f+slope*(1+1),0,0,0,1,0}, v3={-1,1,1.5f-+slope*(-1+1),0,0,0,1,0}, v4={-1,-1,1.5f+slope*(-1-1),0,0,0,0,0};
+
+        auto start_time = std::chrono::high_resolution_clock::now();
+        for (int i=0; i!=100000; ++i) {
+            rasterizer.render_triangle(v1,v2,v3);
+            rasterizer.render_triangle(v4,v1,v3);
+        }
+        auto end_time = std::chrono::high_resolution_clock::now();
+        double elapsed_time = std::chrono::duration<double>(end_time-start_time).count();
+
+        std::cout << "elapsed time: " << elapsed_time << '\n';
+
+
+
+        rasterizer.render_triangle(v1,v2,v3);
+        rasterizer.render_triangle(v4,v1,v3);
+        
+        // print out the screen with a frame around it
+        std::cout << '+';
+        for (int j=0; j!=w; ++j) std::cout << '-';
+        std::cout << "+\n";
+
+        for (int i=0;i!=h;++i) {
+            std::cout << '|';
+            for (int j=0; j!=w; ++j) std::cout << screen[i*w+j];
+            std::cout << "|\n";
+        }
+
+        std::cout << '+';
+        for (int j=0; j!=w; ++j) std::cout << '-';
+        std::cout << "+\n";
+
+        return 0;
+    }
+
